73e8a8f190f0e20c2b4aba65c6e4b776edcb9acd
citoh <francisco@citoh.com.br>
2026-03-23 16:55:06 -0300

    Code review

--- aula5/exercicios/e07_ordernar_lista.py
+++ aula5/exercicios/e07_ordernar_lista.py
@@ -1,1 +1,1 @@
-# Desenvolva um algoritmo capaz de ordenar uma lista de inteiros, sem utilizar o .sort()
+# Desenvolva um algoritmo capaz de ordenar uma lista de inteiros, sem utilizar .sort() ou sorted()
